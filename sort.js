21231e378b62fcfeff8eea27077bb1fd0afb56ec
Shawn Southwell <shawn.southwel@gmail.com>
2017-01-03 18:50:12 -0800

    no string duplicates

---
+++ sort.js
@@ -0,0 +1,16 @@
+let arr = [4,2,1];
+function insertionSort(arr){
+for (let i = 1; i < arr.length; i++){
+	let j = i;
+	while(j > 0 && arr[j-1] > arr[j]){
+		let temp = arr[j - 1];
+		arr[j - 1] = arr[j];
+		arr[j] = temp;
+		--j;
+}
+	
+}	
+return arr;
+
+}
+console.log(insertionSort(arr));
